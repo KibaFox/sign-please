d973186aa5287cf67d02051a7560bd7554002654
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-11-17 15:58:48 -0500

    Produce a minimal "hello-world" json message

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
+import "encoding/json"
 import "fmt"
 
+type SignedMessage struct {
+	Message string `json:"message"`
+}
+
 func main() {
-	fmt.Println("vim-go")
+	msg := &SignedMessage{
+		Message: "hello-world"}
+
+	json, _ := json.Marshal(msg)
+	fmt.Println(string(json))
 }
