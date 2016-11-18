40b7fd258c1a9325e6078a6ce5015891afd4bb9a
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-11-18 00:01:56 -0500

    Sign message, add pubkey, gen or read key file

--- main.go
+++ main.go
@@ -4,22 +4,29 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 type SignedMessage struct {
-	Message string `json:"message"`
+	Message   string `json:"message"`
+	Signature string `json:"signature"`
+	PubKey    string `json:"pubkey"`
 }
 
-func GenerateKey() {
-	priv, _ := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+func GenerateKey() (*ecdsa.PrivateKey, error) {
+	return ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+}
 
-	privDer, _ := x509.MarshalECPrivateKey(priv)
+func SavePrivKey(privKey *ecdsa.PrivateKey, path string) {
+	privDer, _ := x509.MarshalECPrivateKey(privKey)
 
 	privBlk := pem.Block{
 		Type:    "ECDSA PRIVATE KEY",
@@ -27,23 +34,91 @@ func GenerateKey() {
 		Bytes:   privDer,
 	}
 
-	f, _ := os.Create("ecdsa")
+	f, _ := os.Create(path)
 	_ = f.Chmod(os.FileMode(int(0600)))
 	_ = pem.Encode(f, &privBlk)
 	defer f.Close()
 }
 
-func SignMessage(msg string) ([]byte, error) {
+func ReadPrivKey(path string) (*ecdsa.PrivateKey, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	privBlk, _ := pem.Decode(data)
+
+	return x509.ParseECPrivateKey(privBlk.Bytes)
+}
+
+func ReadOrGenKey() (*ecdsa.PrivateKey, error) {
+	path := "ecdsa"
+	key, err := ReadPrivKey(path)
+	if os.IsNotExist(err) {
+		key, err = GenerateKey()
+		if err != nil {
+			return nil, err
+		}
+
+		SavePrivKey(key, path)
+	} else if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func HashMsg(msg string) []byte {
+	hash := sha256.Sum256([]byte(msg))
+
+	// Convert [32]byte to []byte
+	return hash[:]
+}
+
+func SignMsg(msg string, privKey *ecdsa.PrivateKey) (string, error) {
+	var signature []byte
+
+	r, s, err := ecdsa.Sign(rand.Reader, privKey, HashMsg(msg))
+	if err != nil {
+		return "", err
+	}
+
+	signature = append(r.Bytes(), s.Bytes()...)
+	return base64.StdEncoding.EncodeToString(signature), nil
+}
+
+func PublicPem(privKey *ecdsa.PrivateKey) string {
+	pub := privKey.PublicKey
+	pubBytes := append(pub.X.Bytes(), pub.Y.Bytes()...)
+	b64 := base64.StdEncoding.EncodeToString(pubBytes)
+
+	pubBlk := pem.Block{
+		Type:    "ECDSA PUBLIC KEY",
+		Headers: nil,
+		Bytes:   []byte(b64),
+	}
+
+	return string(pem.EncodeToMemory(&pubBlk))
+}
+
+func SignMessage(msg string, privKey *ecdsa.PrivateKey) ([]byte, error) {
 	if len(msg) > 250 {
 		return nil, errors.New("Message can not exceed 250 characters")
 	}
 
+	signature, err := SignMsg(msg, privKey)
+	if err != nil {
+		return nil, err
+	}
+
 	signed := &SignedMessage{
-		Message: msg}
+		Message:   msg,
+		Signature: signature,
+		PubKey:    PublicPem(privKey)}
 
 	signedj, err := json.Marshal(signed)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	return signedj, nil
@@ -55,10 +130,16 @@ func main() {
 		os.Exit(42)
 	}
 
-	GenerateKey()
+	privKey, err := ReadOrGenKey()
+	if err != nil {
+		panic(err)
+	}
+	if privKey == nil {
+		fmt.Println(string("privKey is nil"))
+	}
 
 	arg := os.Args[1]
-	signed, err := SignMessage(arg)
+	signed, err := SignMessage(arg, privKey)
 	if err != nil {
 		panic(err)
 	}
